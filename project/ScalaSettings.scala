56b55b53e39777e47bc7dc78b0ff72d47103fbf4
Tony Morris <tmorris@tmorris.net>
2014-01-15 13:16:07 +1000

    Introduce wartremover after some discussion with Brian.
    Resulted in in issues #8 and #9.

--- project/ScalaSettings.scala
+++ project/ScalaSettings.scala
@@ -9,7 +9,7 @@ object ScalaSettings {
     scalaVersion := "2.10.2"
   , crossScalaVersions := Seq("2.9.2", "2.9.3", "2.10")
   , scalacOptions <++= onVersionTask(
-      all = Seq("-deprecation", "-unchecked", "-optimise")
+      all = Seq("-deprecation", "-unchecked", "-optimise", "-P:wartremover:traverser:org.brianmckenna.wartremover.warts.Unsafe")
     , on210 = Seq("-Yinline-warnings", "-feature", "-language:implicitConversions", "-language:higherKinds", "-language:postfixOps")
     )
   )
