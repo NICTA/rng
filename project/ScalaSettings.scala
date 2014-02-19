76be0969dbed5f46edcafbddcd1d51ec8050a5b9
markhibberd <mark@hibberd.id.au>
2014-02-20 10:46:35 +1100

    Release plugin, and fix compile settings.

--- project/ScalaSettings.scala
+++ project/ScalaSettings.scala
@@ -7,12 +7,12 @@ object ScalaSettings {
 
   lazy val all: Seq[Sett] = Seq(
     scalaVersion := "2.10.3"
-  , crossScalaVersions := Seq("2.9.2", "2.9.3", "2.10")
-  , scalacOptions in compile <++= onVersionTask(
-      all = Seq("-deprecation", "-unchecked", "-optimise", "-P:wartremover:traverser:org.brianmckenna.wartremover.warts.Unsafe")
+  , crossScalaVersions := Seq("2.9.3", "2.10.3")
+  , scalacOptions in Compile <++= onVersionTask(
+    all = Seq("-deprecation", "-unchecked", "-optimise", "-P:wartremover:traverser:org.brianmckenna.wartremover.warts.Unsafe")
     , on210 = Seq("-Yinline-warnings", "-feature", "-language:implicitConversions", "-language:higherKinds", "-language:postfixOps")
     )
-  , scalacOptions in test <++= onVersionTask(
+  , scalacOptions in Test <++= onVersionTask(
       all = Seq("-deprecation", "-unchecked", "-optimise")
     , on210 = Seq("-Yinline-warnings", "-feature", "-language:implicitConversions", "-language:higherKinds", "-language:postfixOps")
     )
--- project/build.scala
+++ project/build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import Tools.onVersion
+import sbtrelease.ReleasePlugin._
 
 object build extends Build {
   type Sett = Project.Setting[_]
@@ -8,21 +9,20 @@ object build extends Build {
   val base = Defaults.defaultSettings ++ ScalaSettings.all ++ Seq[Sett](
       name := "rng"
     , organization := "com.nicta"
-    , version := "1.3-SNAPSHOT"
   )
 
-  val scalaz          = "org.scalaz"       %% "scalaz-core"               % "7.0.0"
-  val scalazEffect    = "org.scalaz"       %% "scalaz-effect"             % "7.0.0"
-  val scalazCheck     = "org.scalaz"       %% "scalaz-scalacheck-binding" % "7.0.0"    % "test"
+  val scalaz          = "org.scalaz"       %% "scalaz-core"               % "7.0.4"
+  val scalazEffect    = "org.scalaz"       %% "scalaz-effect"             % "7.0.4"
+  val scalazCheck     = "org.scalaz"       %% "scalaz-scalacheck-binding" % "7.0.4"    % "test"
   val scalacheck      = "org.scalacheck"   %% "scalacheck"                % "1.10.0"   % "test"
   val specs2_1_12_4_1 = "org.specs2"       %% "specs2"                    % "1.12.4.1" % "test"
   val specs2_2_3_2    = "org.specs2"       %% "specs2"                    % "2.3.2"    % "test"
-  val wartremover     = "org.brianmckenna" %% "wartremover"               % "0.7" 
+  val wartremover     = "org.brianmckenna" %% "wartremover"               % "0.7"
 
   val rng = Project(
     id = "rng"
   , base = file(".")
-  , settings = base ++ ReplSettings.all ++ PublishSettings.all ++ InfoSettings.all ++ Seq[Sett](
+  , settings = base ++ ReplSettings.all ++ releaseSettings ++ PublishSettings.all ++ InfoSettings.all ++ Seq[Sett](
       name := "rng"
     , libraryDependencies <++= onVersion(
         all = Seq(scalaz, scalazEffect, scalazCheck, scalacheck)
