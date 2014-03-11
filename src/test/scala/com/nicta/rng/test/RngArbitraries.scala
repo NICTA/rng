bd8a09911b66fd9c78ec80d83e53869eaa0d2f69
etorreborre <etorreborre@yahoo.com>
2014-03-11 14:24:45 +1100

    fixed the RngArbitraries which was reverted by the previous merge

--- src/test/scala/com/nicta/rng/test/RngArbitraries.scala
+++ src/test/scala/com/nicta/rng/test/RngArbitraries.scala
@@ -3,17 +3,15 @@ package test
 
 import scalaz._, Scalaz._
 import scalaz.scalacheck.ScalazArbitrary._
-import org.scalacheck.{Pretty, Gen, Arbitrary}, Arbitrary.arbitrary, Gen.{frequency, oneOf}
+import org.scalacheck.{Gen, Arbitrary}, Arbitrary.arbitrary, Gen.{frequency, oneOf}
 
 trait RngArbitraries {
-  implicit def ShowPretty[A: Show](a: A): Pretty =
-    Pretty(_ => a.shows)
 
   implicit def RngIntArbitrary: Arbitrary[Rng[Int]] =
-    Arbitrary(Gen(params => Some(Rng.setseed(params.rng.nextInt) >> Rng.int)))
+    Arbitrary(Gen.parameterized(params => Gen.value(Rng.setseed(params.rng.nextInt) >> Rng.int)))
 
   implicit def RngIntFunctionArbitrary: Arbitrary[Rng[Int => Int]] =
-    Arbitrary(Gen(params => Some(Rng.int.function[Int, Int](CoRng(_ * _)))))
+    Arbitrary(Gen.parameterized(params => Gen.value(Rng.int.function[Int, Int](CoRng(_ * _)))))
 
 }
 
